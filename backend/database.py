c5421ec0dc11d6d7832a2c47ebb08239434e86df
SahilNambiar11 <sahil.nambiar11@gmail.com>
2026-06-06 19:17:30 -0400

    database issue

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://sahil:password123@127.0.0.1:5433/minirender"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://sahil:password123@127.0.0.1:5433/minirender"
+)
 
 engine = create_engine(DATABASE_URL)
 
